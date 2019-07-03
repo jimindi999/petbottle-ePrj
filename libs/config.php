42ebbd22d65c145eae4d926828c5eda13cac9fd5
KuroKeita <kuro.keita94@gmail.com>
2019-07-03 19:24:02 +0700

    edit port

--- libs/config.php
+++ libs/config.php
@@ -5,7 +5,7 @@
 	include("Model.class.php");
 	include("app.class.php");
 	$baseUrl = "http://".$_SERVER['HTTP_HOST']."/";
-	$dsn="mysql:host=localhost;port=33060;dbname=petbottle";
+	$dsn="mysql:host=localhost;port=3306;dbname=petbottle";
 	$usr = 'kurokeita';
 	$pwd = 'harunonaru';
 	$db = new Model($dsn,$usr,$pwd);
