57ed244937d1b1a63af377a385ed01b54b90a792
KuroKeita <kuro.keita94@gmail.com>
2019-07-11 14:17:47 +0700

    update logout script (destroy username cookie)

--- admin/controllers/logout.php
+++ admin/controllers/logout.php
@@ -6,6 +6,7 @@
         $_SESSION = array();
         //Destroy cookie, set limit time to 86400s before current time
         setcookie("user_id", "", time() - 86400, "/");
+        setcookie("username", "", time() - 86400, "/");
         setcookie("admin", "", time() - 86400, "/");
         session_destroy();
         $f->redir('index.php');
