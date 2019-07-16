c97ff50a654cb008f09400f5ce54ca9bfcd95f44
KuroKeita <kuro.keita94@gmail.com>
2019-07-17 05:53:21 +0700

    edit css for homepage and admin/index.html + admin/navbar

--- admin/controllers/logout.php
+++ admin/controllers/logout.php
@@ -5,9 +5,10 @@
         session_unset();
         $_SESSION = array();
         //Destroy cookie, set limit time to 86400s before current time
-        setcookie("user_id", "", time() - 86400, "/");
-        setcookie("username", "", time() - 86400, "/");
-        setcookie("admin", "", time() - 86400, "/");
+        setcookie("admin_id", "", time() - 86400, "/admin/");
+        setcookie("admin_username", "", time() - 86400, "/admin/");
+        setcookie("admin_level", "", time() - 86400, "/admin/");
         session_destroy();
+        // echo $_COOKIE['admin_id'];
         $f->redir('index.php');
     }
